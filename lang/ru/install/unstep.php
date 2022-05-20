d64f0c3cf955961376ce32c3b99ece5f2c1f9020
Vyacheslav Petukhov <sadn3r@gmail.com>
2022-05-20 10:15:26 +0400

    Update unstep.php

--- lang/ru/install/unstep.php
+++ lang/ru/install/unstep.php
@@ -1,5 +1,5 @@
 <?php
-$moduleId = getModuleId(__DIR__);
+$moduleId = "bitrix.bootstrapmodule";
 $MESS[$moduleId . '_UNSTEP_BEFORE'] = 'Модуль';
 $MESS[$moduleId . '_UNSTEP_AFTER'] = 'удален';
 $MESS[$moduleId . '_UNSTEP_SUBMIT_BACK'] = 'Назад к списку';
